2c4fe57459c9e52338536e92a7cb7243740efbb1
Hiroki Katayama <katayama.hiroki.kb3@is.naist.jp>
2021-07-14 13:30:19 +0900

    fix data balanser

--- script/data_num_balanser.py
+++ script/data_num_balanser.py
@@ -18,7 +18,6 @@ def reset_dict(dic):
 def voc_label_dist(data_path):
     key_open = open('./data_num.json', 'r')
     key_json = json.load(key_open)
-    old_img_path = "none"
 
     label_dist = reset_dict(key_json)
     for label_path in glob.glob(data_path + '/Annotations/*'):
@@ -31,56 +30,69 @@ def voc_label_dist(data_path):
     return label_dist
 
 
-def check_destination(dest, now):
+def check_destination(dest, output_path):
+    labels = voc_label_dist(output_path)
     for i in dest:
-        if dest[i] > now[i]:
+        if dest[i] > labels[i]:
             return 1
         else:
             continue
     return 0
 
 
-def save_img_xml(label_tree, output_path, label_path, cp_count):
-    label_tree.write(output_path + '/Annotations/' +
-                     Path(label_path).stem + '-' + str(cp_count) + '.xml')
+def check_labels(label_root, enough_labels):
+    for bb in label_root.findall('object'):
+        # print(i.text)
+        label = bb.find('name').text
+        if label in enough_labels:
+            print("skip")
+            return 0
+    return 1
+
+
+def update_enough_labels(output_path, enough_labels):
+    key_open = open('./data_num.json', 'r')
+    key_json = json.load(key_open)
+
+    labels = voc_label_dist(output_path)
+    print("after", voc_label_dist(output_path))
+    for label in labels:
+        if label in enough_labels:
+            continue
+        if labels[label] >= key_json[label]:
+            enough_labels.append(label)
+    return enough_labels
 
 
 def voc_copy_imgs(data_path, output_path):
     key_open = open('./data_num.json', 'r')
     key_json = json.load(key_open)
-    old_img_path = "none"
 
     origin_label_dist = voc_label_dist(data_path)
-    add_label_dist = reset_dict(origin_label_dist.copy())
+    # add_label_dist = reset_dict(origin_label_dist.copy())
 
     cp_count = 1
+    enough_labels = []
     print("before", origin_label_dist)
-    while check_destination(key_json, add_label_dist):
+    while check_destination(key_json, output_path):
         for label_path in glob.glob(data_path + '/Annotations/*'):
             label_tree = ET.parse(label_path)
             label_root = label_tree.getroot()
-            for bb in label_root.findall('object'):
-                # print(i.text)
-                label = bb.find('name').text
-                if add_label_dist[label] < key_json[label]:
-                    add_label_dist[label] += 1
-                else:
-                    label_root.remove(bb)
-            ants_path = output_path + '/Annotations/' + \
-                Path(label_path).stem + '-'
-            imgs_path = output_path + '/JPEGImages/' + \
-                Path(label_path).stem + '-'
-            img_path = label_path.replace(
-                '.xml', '.jpg').replace('Annotations', 'JPEGImages')
-            # print(img_path)
-            if label_root.findall('object'):
-                label_tree.write(ants_path + str(cp_count) + '.xml')
-                shutil.copy(img_path, imgs_path + str(cp_count) + '.jpg')
-            elif add_label_dist['no-obj'] < key_json['no-obj']:
-                label_tree.write(ants_path + str(cp_count) + '.xml')
-                shutil.copy(img_path, imgs_path + str(cp_count) + '.jpg')
-                add_label_dist['no-obj'] += 1
-        print("after", add_label_dist)
+            if check_labels(label_root, enough_labels):
+                ants_path = output_path + '/Annotations/' + \
+                    Path(label_path).stem + '-'
+                imgs_path = output_path + '/JPEGImages/' + \
+                    Path(label_path).stem + '-'
+                img_path = label_path.replace(
+                    '.xml', '.jpg').replace('Annotations', 'JPEGImages')
+                # print(img_path)
+                if label_root.findall('object'):
+                    label_tree.write(ants_path + str(cp_count) + '.xml')
+                    shutil.copy(img_path, imgs_path + str(cp_count) + '.jpg')
+                elif 'no-obj' not in enough_labels:
+                    label_tree.write(ants_path + str(cp_count) + '.xml')
+                    shutil.copy(img_path, imgs_path + str(cp_count) + '.jpg')
+        enough_labels = update_enough_labels(output_path, enough_labels)
         cp_count += 1
     print("finish", voc_label_dist(output_path))
 
